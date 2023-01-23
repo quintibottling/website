6b03ae3854eb4e521d4ba49d7df5d4fcc66036d9
Guido Squillace <g.squillace@cantierecreativo.net>
2023-01-23 09:37:40 +0100

    Edit block certification

--- components/CertificationCard.js
+++ components/CertificationCard.js
@@ -6,18 +6,18 @@ export default function CertificationCard({ locale, item }) {
     <>
       <div>
         <div className="relative grid gap-4 rounded-[20px] bg-[#F2F0EB] p-4 py-6 lg:px-8 lg:py-12">
-          <div className="relative h-[105px] w-[105px] xl:h-[150px] xl:w-[150px]">
-            <Image
-              src={item.logo.url}
-              layout="fill"
-              alt={item.logo.alt}
-              title={item.logo.title}
-            />
-          </div>
-          <h3 className="text-lg text-gold lg:mt-6 lg:text-2xl">
-            {item.title}
-          </h3>
-          <div className="opacity-9 text-sm lg:text-base">
+          {item.logo && (
+            <div className="relative h-[105px] w-[105px] xl:h-[150px] xl:w-[150px]">
+              <Image
+                src={item.logo.url}
+                layout="fill"
+                alt={item.logo.alt}
+                title={item.logo.title}
+              />
+            </div>
+          )}
+          <h3 className="text-lg text-gold lg:text-2xl">{item.title}</h3>
+          <div className="opacity-9 formatted text-sm lg:text-base">
             {renderHTML(item.text)}
           </div>
         </div>
